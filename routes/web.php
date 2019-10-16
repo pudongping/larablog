81035f142bc81ccb099c16541306caa694162178
Alex <1414818093@qq.com>
2019-10-17 00:29:52 +0800

    修改用户授权相关路由

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,32 @@
 |
 */
 
-Auth::routes();
-
 // 首页路由
 Route::get('/', 'Portal\HomeController@root')->name('root');
-
-
+// 测试路由
 Route::resource('tests', 'TestsController');
+
+/**
+ * 用户相关路由
+ * Auth::routes();
+ * 等同于以下路由
+ * 可以在 vendor/laravel/framework/src/Illuminate/Routing/Router.php 中搜索关键词 LoginController 即可找到定义的地方
+ */
+// 用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+// 用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+// 密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+// Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
 
