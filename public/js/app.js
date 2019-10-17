53082fb08098ae5000ed75667bd65ac30a4bd651
alex <1414818093@qq.com>
2019-10-17 08:47:44 +0800

    npm i startbootstrap-sb-admin-2 安装皮肤框架

--- public/js/app.js
+++ public/js/app.js
@@ -36973,8 +36973,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/larablog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/larablog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! G:\laragon\wwwroot\larablog\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! G:\laragon\wwwroot\larablog\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
