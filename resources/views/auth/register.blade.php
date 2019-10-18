9a9bb7f2389e1d74939808b10605fb9ed3ac2892
alex <1414818093@qq.com>
2019-10-18 14:20:00 +0800

    注册用户页面

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,57 +3,89 @@
 @section('content')
 
   <div class="card o-hidden border-0 shadow-lg my-5">
+
     <div class="card-body p-0">
+
       <!-- Nested Row within Card Body -->
       <div class="row">
+
         <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+
         <div class="col-lg-7">
+
           <div class="p-5">
+
             <div class="text-center">
-              <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+            <h1 class="h4 text-gray-900 mb-4">{{ __('Create an Account!') }}</h1>
             </div>
-            <form class="user">
-              <div class="form-group row">
-                <div class="col-sm-6 mb-3 mb-sm-0">
-                  <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
-                </div>
-                <div class="col-sm-6">
-                  <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
-                </div>
-              </div>
+
+            <form class="user" method="POST" action="{{ route('register') }}">
+              @csrf
+
+              {{-- 姓名 --}}
               <div class="form-group">
-                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                  <input type="text" class="form-control form-control-user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus id="exampleFirstName" placeholder="{{ __('Name') }}">
+                  @error('name')
+                  <div class="text-center " style="color: #d9534f">{{ $message }}</div>
+                  @enderror
               </div>
+
+              {{-- 邮箱地址 --}}
+              <div class="form-group">
+                <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" id="exampleInputEmail" placeholder="{{ __('E-Mail Address') }}">
+                @error('email')
+                <div class="text-center " style="color: #d9534f">{{ $message }}</div>
+                @enderror
+              </div>
+
               <div class="form-group row">
                 <div class="col-sm-6 mb-3 mb-sm-0">
-                  <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                  <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" id="exampleInputPassword" placeholder="{{ __('Password') }}">
+                  @error('password')
+                  <div class="text-center " style="color: #d9534f">{{ $message }}</div>
+                  @enderror
                 </div>
                 <div class="col-sm-6">
-                  <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                  <input type="password" class="form-control form-control-user" name="password_confirmation" required autocomplete="new-password" id="exampleRepeatPassword" placeholder="{{ __('Confirm Password') }}">
                 </div>
               </div>
-              <a href="login.html" class="btn btn-primary btn-user btn-block">
-                Register Account
-              </a>
+
+              <button type="submit" class="btn btn-primary btn-user btn-block">
+                {{ __('Register Account') }}
+              </button>
+
               <hr>
+
               <a href="index.html" class="btn btn-google btn-user btn-block">
                 <i class="fab fa-google fa-fw"></i> Register with Google
               </a>
+
               <a href="index.html" class="btn btn-facebook btn-user btn-block">
                 <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
               </a>
+
             </form>
+
             <hr>
+
             <div class="text-center">
-              <a class="small" href="forgot-password.html">Forgot Password?</a>
+              @if (Route::has('password.request'))
+                <a class="small" href="{{ route('password.request') }}">{{ __('Forgot Password?') }}</a>
+              @endif
             </div>
+
             <div class="text-center">
-              <a class="small" href="login.html">Already have an account? Login!</a>
+              <a class="small" href="{{ route('login') }}">{{ __('Already have an account? Please Login!') }}</a>
             </div>
+
           </div>
+
         </div>
+
       </div>
+
     </div>
+
   </div>
 
 @endsection
