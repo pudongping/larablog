d4ae42962439822c2fc92b151227daf800c45d2f
alex <1414818093@qq.com>
2019-10-18 14:41:33 +0800

    登录页面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,50 +8,84 @@
     <div class="col-xl-10 col-lg-12 col-md-9">
 
       <div class="card o-hidden border-0 shadow-lg my-5">
+
         <div class="card-body p-0">
+
           <!-- Nested Row within Card Body -->
           <div class="row">
+
             <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
             <div class="col-lg-6">
+
               <div class="p-5">
+
                 <div class="text-center">
                   <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                 </div>
-                <form class="user">
+
+                <form class="user" method="POST" action="{{ route('login') }}">
+                  @csrf
+
+                  {{-- 邮箱 --}}
                   <div class="form-group">
-                    <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus id="exampleInputEmail" aria-describedby="emailHelp" placeholder="请输入您的邮箱地址">
+                    @error('email')
+                    <div class="text-center " style="color: #d9534f">{{ $message }}</div>
+                    @enderror
                   </div>
+
+                  {{-- 密码 --}}
                   <div class="form-group">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" id="exampleInputPassword" placeholder="请输入密码">
+                    @error('password')
+                    <div class="text-center " style="color: #d9534f">{{ $message }}</div>
+                    @enderror
                   </div>
+
+                  {{-- 记住我？ --}}
                   <div class="form-group">
                     <div class="custom-control custom-checkbox small">
-                      <input type="checkbox" class="custom-control-input" id="customCheck">
-                      <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      <input type="checkbox" class="custom-control-input" id="customCheck" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                      <label class="custom-control-label" for="customCheck">{{ __('Remember Me') }}</label>
                     </div>
                   </div>
-                  <a href="index.html" class="btn btn-primary btn-user btn-block">
-                    Login
-                  </a>
+
+                  <button type="submit" class="btn btn-primary btn-user btn-block">
+                    {{ __('Login') }}
+                  </button>
+
                   <hr>
+
                   <a href="index.html" class="btn btn-google btn-user btn-block">
                     <i class="fab fa-google fa-fw"></i> Login with Google
                   </a>
+
                   <a href="index.html" class="btn btn-facebook btn-user btn-block">
                     <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                   </a>
+
                 </form>
+
                 <hr>
+
                 <div class="text-center">
-                  <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  @if (Route::has('password.request'))
+                    <a class="small" href="{{ route('password.request') }}">{{ __('Forgot Password?') }}</a>
+                  @endif
                 </div>
+
                 <div class="text-center">
-                  <a class="small" href="register.html">Create an Account!</a>
+                  <a class="small" href="{{ route('register') }}">{{ __('Create an Account!') }}</a>
                 </div>
+
               </div>
+
             </div>
+
           </div>
+
         </div>
+
       </div>
 
     </div>
