08dddd54d735f5d1e55e9079ebfb8a5593dea566
Alex <1414818093@qq.com>
2019-10-19 23:28:31 +0800

    users表新增「avatar、introduction」头像、简介字段

---
+++ database/migrations/2019_10_19_232053_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable()->after('email')->comment('头像');
+            $table->string('introduction')->nullable()->after('avatar')->comment('个人简介');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('introduction');
+        });
+    }
+}
