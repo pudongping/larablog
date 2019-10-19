71247b5078edc32e1fa3cf55a543f24c4960e76d
Alex <1414818093@qq.com>
2019-10-20 00:47:39 +0800

    表单参数验证失败提示

---
+++ resources/views/shared/_error.blade.php
@@ -0,0 +1,11 @@
+{{-- 参数验证错误提示 --}}
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <div class="mt-2"><b>有错误发生：</b></div>
+        <ul class="mt-2 mb-2">
+            @foreach ($errors->all() as $error)
+                <li><i class="glyphicon glyphicon-remove"></i> {{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
