8dab90d0f6267281e20419f4745e4e2d683e8157
alex <1414818093@qq.com>
2019-10-19 18:20:19 +0800

    定义用户相关路由

---
+++ app/Http/Controllers/Auth/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Auth\User;
+
+class UsersController extends Controller
+{
+
+    /**
+     * 个人信息显示页面
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        dump($user);die;
+        return view('auth.users.show', compact('user'));
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -54,5 +54,10 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 // 3、点击 「验证 Email」 按钮，验证邮箱（在指定邮箱中可见此按钮），返回认证前的当前页面
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 
-
+// 『用户相关路由』
+Route::resource('users', 'Auth\UsersController', ['only' => ['show', 'update', 'edit']]);
+// 等同于
+// Route::get('/users/{user}', 'Auth\UsersController@show')->name('users.show');  // 显示用户个人信息页面
+// Route::get('/users/{user}/edit', 'Auth\UsersController@edit')->name('users.edit');    // 显示编辑个人资料页面
+// Route::patch('/users/{user}', 'Auth\UsersController@update')->name('users.update');   // 处理 edit 页面提交的更改
 
