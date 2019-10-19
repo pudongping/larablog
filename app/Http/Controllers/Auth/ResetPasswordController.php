87a13a05259483f1ba8ef019ae664c106eef77a0
alex <1414818093@qq.com>
2019-10-19 17:08:25 +0800

    密码重置成功给予成功提示「重写sendResetResponse Trait」方法

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -36,4 +37,21 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /**
+     * 密码重置成功后的响应
+     * 重写了 「ResetsPasswords」 中的 「sendResetResponse」 方法
+     * trait 优先级：当前类的成员 > trait > extends
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath())
+            ->with('status', trans($response));
+    }
+
 }
