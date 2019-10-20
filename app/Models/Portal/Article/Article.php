5ed7542bc917e44c7e976c0255d0bb486d191e89
Alex <1414818093@qq.com>
2019-10-20 18:18:46 +0800

    文章模型指定修改字段

--- app/Models/Portal/Article/Article.php
+++ app/Models/Portal/Article/Article.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    /**
+     * 允许修改的字段
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'body', 'category_id', 'excerpt', 'slug'
+    ];
 }
