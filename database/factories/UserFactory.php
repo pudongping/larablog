3aa56f956a192fd74597260cddcb4a6f2b01dbc2
Alex <1414818093@qq.com>
2019-10-20 18:47:03 +0800

    填充用户假数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,14 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'password' => bcrypt(123456789), // secret
+        'introduction' => $faker->sentence(), // 随机生成『小段落』文本
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
