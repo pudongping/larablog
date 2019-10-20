f4ae04ee4afd9b6921800bc6c130712d3cdf4500
Alex <1414818093@qq.com>
2019-10-20 17:49:28 +0800

    分类数据默认值填充

---
+++ database/migrations/2019_10_20_174348_seed_categories_data.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '分享',
+                'description' => '分享创造，分享技巧',
+            ],
+            [
+                'name'        => '教程',
+                'description' => '开发技巧、推荐扩展包等',
+            ],
+            [
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
