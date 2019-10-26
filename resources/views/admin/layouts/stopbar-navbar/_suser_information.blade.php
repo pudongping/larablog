2af44e5bbb3cd4a40e125ce65ef2a65fd55575f7
Alex <1414818093@qq.com>
2019-10-27 00:02:08 +0800

    用户头像下拉增加「管理后台」按钮

--- resources/views/admin/layouts/stopbar-navbar/_suser_information.blade.php
+++ resources/views/admin/layouts/stopbar-navbar/_suser_information.blade.php
@@ -19,6 +19,13 @@
             访问日志
         </a>
         <div class="dropdown-divider"></div>
+        @can('manage_contents')
+            <a class="dropdown-item" href="{{ route('dashboard') }}">
+                <i class="fas fa-tachometer-alt mr-2"></i>
+                管理后台
+            </a>
+            <div class="dropdown-divider"></div>
+        @endcan
         <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
             退出
