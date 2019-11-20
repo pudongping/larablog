d57c47e8953df96548cb02d00bfe87f7c602ac58
Alex <1414818093@qq.com>
2019-11-20 16:22:44 +0800

    创建「用户-粉丝」关联表

---
+++ database/migrations/2019_11_20_151541_create_fans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->index()->comment('被关注人的 id');
+            $table->integer('fan_id')->index()->comment('粉丝 id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fans');
+    }
+}
