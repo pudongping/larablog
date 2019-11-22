07f81758d292eba228db5737df975c17e5b69775
Alex <1414818093@qq.com>
2019-11-22 23:53:42 +0800

    优化消息提示弹框

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,9 +1,11 @@
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
     @if(session()->has($msg))
-        <div class="flash-message">
-            <p class="alert alert-{{ $msg }}">
-                {{ session()->get($msg) }}
-            </p>
+        
+        <div class="alert alert-{{$msg}} alert-dismissible flash-message" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <i class="fa fa-check-circle fa-lg fa-fw"></i>
+            <strong style="text-transform: capitalize">{{$msg}}!</strong> {{ session()->get($msg) }}
         </div>
+
     @endif
 @endforeach
