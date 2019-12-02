4ceb449e73938c9ca1564c52678e6f556344e7e8
Alex <1414818093@qq.com>
2019-12-03 00:26:46 +0800

    默认第一个用户跳过所有权限

--- app/Http/Middleware/CheckAdminMenusMiddleware.php
+++ app/Http/Middleware/CheckAdminMenusMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\Auth\User;
 
 class CheckAdminMenusMiddleware
 {
@@ -15,22 +16,29 @@ class CheckAdminMenusMiddleware
      */
     public function handle($request, Closure $next)
     {
-        // 如果当前请求的 url 没有设置路由别名，则直接跳过
-        if (! \Route::currentRouteName()) {
-            return $next($request);
-        }
 
-        // 当前请求的 url 的别名，比如：menus.index
-        $currentRouteName = \Route::currentRouteName();
+        // 当前用户表中所有用户数量
+        $users = User::all()->count();
 
-        // 查询当前请求的路由是否设置了权限
-        $menuAuth = \DB::table('menus')->where('link', $currentRouteName)->value('auth');
+        // 如果用户表中只有一个用户，那么则跳过权限判断
+        if (1 <> $users) {
+            // 如果当前请求的 url 没有设置路由别名，则直接跳过
+            if (! \Route::currentRouteName()) {
+                return $next($request);
+            }
 
-        if ($menuAuth) {
-            // 检查用户是否有相应权限
-            $isAuth = \Auth::user()->hasPermissionTo($menuAuth);
-            // 如果没有相应权限则提示 401
-            if (! $isAuth) abort(401);
+            // 当前请求的 url 的别名，比如：menus.index
+            $currentRouteName = \Route::currentRouteName();
+
+            // 查询当前请求的路由是否设置了权限
+            $menuAuth = \DB::table('menus')->where('link', $currentRouteName)->value('auth');
+
+            if ($menuAuth) {
+                // 检查用户是否有相应权限
+                $isAuth = \Auth::user()->hasPermissionTo($menuAuth);
+                // 如果没有相应权限则提示 401
+                if (! $isAuth) abort(401);
+            }
         }
 
         return $next($request);
